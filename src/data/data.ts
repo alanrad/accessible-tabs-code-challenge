92b8673d759174739cda720e6b973fee34147f92
Alan Rad <alan.rad@emsbk.com>
2021-01-26 09:31:16 +1100

    feat: add local data for dev

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,24 +1,45 @@
 export const data = {
-    "tabGroup": [
-        {
-            "id": "react",
-            "label": "React",
-            "content": "Lots of people use React as the V in MVC. Since React makes no assumptions about the rest of your technology stack, it's easy to try it out on a small feature in an existing project."
-        },
-        {
-            "id": "angular",
-            "label": "Angular",
-            "content": "HTML is great for declaring static documents, but it falters when we try to use it for declaring dynamic views in web-applications. AngularJS lets you extend HTML vocabulary for your application. The resulting environment is extraordinarily expressive, readable, and quick to develop."
-        },
-        {
-            "id": "ember",
-            "label": "Ember",
-            "content": "An open-source JavaScript application framework, based on the model-view-controller (MVC) pattern. It allows developers to create scalable single-page web applications by incorporating common idioms and best practices into the framework."
-        },
-        {
-            "id": "backbone",
-            "label": "Backbone",
-            "content": "Backbone JS gives structure to web applications by providing models with key-value binding and custom events, collections with a rich API of enumerable functions, views with declarative event handling, and connects it all to your existing API over a RESTful JSON interface."
-        }
-    ]
-}
+  appName: 'Accessible Tabs Code Challenge',
+  examples: [
+    {
+      id: 'tabs-width-automatic-activation',
+      title: 'Tabs With Automatic Activation',
+      description:
+        'A tabs widget where tabs are automatically activated and their panel is displayed when they receive focus.',
+      activeTab: 'react',
+    },
+    {
+      id: 'tabs-width-manual-activation',
+      title: 'Tabs With Manual Activation',
+      description:
+        'A tabs widget where users activate a tab and display its panel by pressing Space or Enter.',
+      activeTab: 'react',
+    },
+  ],
+  tabGroup: [
+    {
+      id: 'react',
+      label: 'React',
+      content:
+        "Lots of people use React as the V in MVC. Since React makes no assumptions about the rest of your technology stack, it's easy to try it out on a small feature in an existing project.",
+    },
+    {
+      id: 'angular',
+      label: 'Angular',
+      content:
+        'HTML is great for declaring static documents, but it falters when we try to use it for declaring dynamic views in web-applications. AngularJS lets you extend HTML vocabulary for your application. The resulting environment is extraordinarily expressive, readable, and quick to develop.',
+    },
+    {
+      id: 'ember',
+      label: 'Ember',
+      content:
+        'An open-source JavaScript application framework, based on the model-view-controller (MVC) pattern. It allows developers to create scalable single-page web applications by incorporating common idioms and best practices into the framework.',
+    },
+    {
+      id: 'backbone',
+      label: 'Backbone',
+      content:
+        'Backbone JS gives structure to web applications by providing models with key-value binding and custom events, collections with a rich API of enumerable functions, views with declarative event handling, and connects it all to your existing API over a RESTful JSON interface.',
+    },
+  ],
+};
