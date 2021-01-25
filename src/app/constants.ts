5667205326a3b6cc748a33bfd3156c591dddb73e
Alan Rad <alan.rad@emsbk.com>
2021-01-26 09:27:00 +1100

    feat: basic routing and app constants

---
+++ src/app/constants.ts
@@ -0,0 +1,8 @@
+// sharable app constants which might become dynamic las the app grows
+
+// app routes
+export const DETAILS_ROUTE = 'tab';
+
+// tab groups
+export const AUTOMATIC_ACTIVATION = 'tabs-width-automatic-activation';
+export const MANUAL_ACTIVATION = 'tabs-width-manual-activation';
---
+++ src/app/interfaces.ts
@@ -0,0 +1,20 @@
+// sharabale interfaces defined for the application
+
+export interface IappData {
+  appName: string;
+  examples: Iexample[];
+  tabGroup: ItabContent[];
+}
+
+export interface Iexample {
+  id: string;
+  title: string;
+  description: string;
+  activeTab: string;
+}
+
+export interface ItabContent {
+  id: string;
+  label: string;
+  content: string;
+}
